2d5bdd2a24edea09a3fa3a4a287ec273f714fc36
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 20:09:45 -0700

    test

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 inputvalues = input('Enter 5 elements values in ascending order: ')
-
 numbers = inputvalues.split()
 for i in range(len(numbers)):
 	numbers[i] = int(numbers[i]) 
+    
+while sorted(numbers) != numbers or len(numbers) > 5:
+    print('Invalid input')
+    inputvalues = input('Enter 5 elements values in ascending order: ')
+    numbers = inputvalues.split()
+    for i in range(len(numbers)):
+	    numbers[i] = int(numbers[i]) 
 
-insert_number = int(input('Enter the number to insert'))
+insert_number = int(input('Enter the number to insert: '))
 
 for i in range(len(numbers)):
     if insert_number < numbers[i]:
         numbers.insert(i, insert_number)
         break
+    if i == 4:
+        numbers.insert(i, insert_number)
 
 print(numbers)
