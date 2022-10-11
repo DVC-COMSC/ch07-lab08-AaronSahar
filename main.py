2be8077992eb9ac3d404aedf34d9edab2014a4eb
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 20:22:00 -0700

    test

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ for i in range(len(numbers)):
     if insert_number < numbers[i]:
         numbers.insert(i, insert_number)
         break
+    if i == 4:
+        numbers.insert(i+1, insert_number)
 for i in range(len(numbers)):
     print(numbers[i], end=' ')
