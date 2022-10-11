05c264d4db0071070628f15b5b7dd579907aac74
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 20:22:33 -0700

    test

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ inputvalues = input()
 numbers = inputvalues.split()
 for i in range(len(numbers)):
 	numbers[i] = int(numbers[i]) 
-    
-
 insert_number = int(input())
-
 for i in range(len(numbers)):
     if insert_number < numbers[i]:
         numbers.insert(i, insert_number)
